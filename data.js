49c1d9821e683a0b4da70a8e8be4fae202c0d01c
Sherlock2-4 <115625786+Sherlock2-4@users.noreply.github.com>
2022-11-19 19:43:40 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+        name: 'Alive',
+        path: 'assets/musics/Alive.mp3',
+        artist: 'Warbly Jet',
+        cover: 'assets/images/cover 1.jpg'
+    },
+    {
+        name: 'Closer Faster',
+        path: 'assets/musics/Closer Faster.mp3',
+        artist: 'Against the current',
+        cover: 'assets/images/cover 2.jpg'
+    },
+    {
+        name: 'Alive',
+        path: 'assets/musics/Bright.mp3',
+        artist: 'Echosmith',
+        cover: 'assets/images/cover 3.jpg'
+    },
+    {
+        name: 'Alive',
+        path: 'assets/musics/Rebel Rebel.mp3',
+        artist: 'David Bowie',
+        cover: 'assets/images/cover 4.jpg'
+    },
+    {
+        name: 'Alive',
+        path: 'assets/musics/Alive.mp3',
+        artist: 'Warbly Jet',
+        cover: 'assets/images/cover 5.jpg'
+    },
+    {
+        name: 'Alive',
+        path: 'assets/musics/Alive.mp3',
+        artist: 'Warbly Jet',
+        cover: 'assets/images/cover 6.jpg'
+    },
+    {
+        name: 'Alive',
+        path: 'assets/musics/Alive.mp3',
+        artist: 'Warbly Jet',
+        cover: 'assets/images/cover 7.jpg'
+    },
+    {
+        name: 'Alive',
+        path: 'assets/musics/Alive.mp3',
+        artist: 'Warbly Jet',
+        cover: 'assets/images/cover 8.jpg'
+    },
+]
