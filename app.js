9a735d4e49e12a343cc03b19159d8948bec086af
Sherlock2-4 <115625786+Sherlock2-4@users.noreply.github.com>
2023-03-07 18:31:44 +0800

    modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -214,14 +214,3 @@ queue.forEach((item, i) => {
         playBtn.click();
     })
 })	
-
-const playlist = ['Saan Man Patungo', 'When i Dream About You', 'Song 3', 'Song 4', 'Song 5'];
-
-function shufflePlaylist() {
-  for (let i = playlist.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [playlist[i], playlist[j]] = [playlist[j], playlist[i]];
-  }
-  console.log('Shuffled playlist:', playlist);
-  // code to play shuffled playlist
-}
