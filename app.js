936c28de1d5c72a34a1d88650bc38f6b74a71744
Sherlock2-4 <115625786+Sherlock2-4@users.noreply.github.com>
2023-03-07 19:34:20 +0800

    Changes to be committed:

--- app.js
+++ app.js
@@ -213,3 +213,14 @@ queue.forEach((item, i) => {
         playBtn.click();
     })
 })	
+function randomTrack(){
+    isRandom ? pauseRandom() : playRandom();
+}
+function playRandom(){
+    isRandom = true;
+    randomIcon.classList.add('randomActive');
+}
+function pauseRandom(){
+    isRandom = false;
+    randomIcon.classList.remove('randomActive');
+}
